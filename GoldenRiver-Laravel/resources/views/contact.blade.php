bada269d566b9269af2b32ae81e6c32a0787be8b
rayyan2000 <rayyankhan2000@live.com>
2023-02-17 16:22:05 +0000

    added contact page html

---
+++ GoldenRiver-Laravel/resources/views/contact.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Contact Us</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <main>
+      <div class="content">
+        <h1>Contact Us</h1>
+        <p>We would love to respond to your queries. Feel free to get in touch with us.</p>
+        <form action="#">
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name" required>
+
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" required>
+
+          <label for="message">Message</label>
+          <textarea id="message" name="message" required></textarea>
+
+          <input type="submit" value="Send">
+        </form>
+      </div>
+      <div class="reach-us">
+        <p><strong>Reach Us</strong></p>
+        <p>Email: info@jewellerystore.com</p>
+        <p>Phone: +1 123 456 7890</p>
+      </div>
+    </main>
+  </body>
+</html>
--- GoldenRiver-Laravel/routes/web.php
+++ GoldenRiver-Laravel/routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('userRegistration');
 });
 
+Route::get('/contact', function () {
+    return view('contact');
+});
+
 //test
 Route::get('/userRegistration', [RegistrationController::class , 'show']);
 
