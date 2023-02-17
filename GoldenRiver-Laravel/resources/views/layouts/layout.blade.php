957f0f1346a00039a039e7d687d18080be24955e
Cyberrrrrrr <goodcyberengine@gmail.com>
2023-02-17 15:27:19 +0000

    commit navbar and header, rename master files.
    renamed master files;
    
    master.css -> style.css
    master.js -> index.css

--- GoldenRiver-Laravel/resources/views/layouts/layout.blade.php
+++ GoldenRiver-Laravel/resources/views/layouts/layout.blade.php
@@ -6,23 +6,16 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
-    <!-- JavaScript Bundle with Popper -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="/css/master.css" type="text/css">
-    <script src="/js/master.js"></script>
-    
-    @yield('css')
+<!-- footer section starts-->
+@yield('css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+@section('css')
+<!--all the links for style sheets custom and ready made bootstrap-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css%22%3E
+<script src="https://kit.fontawesome.com/3cc03d8fde.js" crossorigin="anonymous"></script>
+@endsection
+
     <!-- Styles -->
 </head>
     <body>
