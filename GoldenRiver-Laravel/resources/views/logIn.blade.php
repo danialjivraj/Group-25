e1615239b192923507f70c9a64a2ca2ce3d9f2d9
fahm781 <farazahmed9910@gmail.com>
2023-02-22 21:23:41 +0000

    Fixed some errors

--- GoldenRiver-Laravel/resources/views/logIn.blade.php
+++ GoldenRiver-Laravel/resources/views/logIn.blade.php
@@ -6,7 +6,6 @@
 
 @section('css')
     <link rel="stylesheet" href="app.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <script src="./login.js"></script>
 @section('css')
 
