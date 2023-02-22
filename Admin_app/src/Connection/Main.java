383a4ab6764ebba6e9d6978eba6eaa8cd4825c08
UshakovKirill <220036159@aston.ac.uk>
2023-02-22 22:40:57 +0000

    Check
    check

--- Admin_app/src/Connection/Main.java
+++ Admin_app/src/Connection/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		//DBorder dba=new DBorder();
-		
+		//test
 		//Date date=new Date(31,03,2002);
 		//dba.createRecordAccount("1234567890","ush@mail.ru","Kirill","Ushakov","Male",new Date(2002,03,31),1,"TestPass");
 		
