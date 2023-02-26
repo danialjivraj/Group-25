ab0db5f5690c7daea2f07c766b76b226e14e6a95
rayyan2000 <rayyankhan2000@live.com>
2023-02-26 23:49:30 +0000

    Added CSS for the FAQ page

--- GoldenRiver-Laravel/resources/views/faq.blade.php
+++ GoldenRiver-Laravel/resources/views/faq.blade.php
@@ -6,7 +6,7 @@
 
 @section('css')
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('css/contact.css')}}">
+<link rel="stylesheet" href="{{asset('css/faq.css')}}">
 @endsection
 
 
@@ -52,4 +52,5 @@
 	</section>
 	<script src="faq.js"></script>
 </body>
+@endsection
 </html>
