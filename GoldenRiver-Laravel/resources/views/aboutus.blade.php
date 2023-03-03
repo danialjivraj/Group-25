513691ca1028ff0c4e2c72d6537766d4196399ae
rayyan2000 <rayyankhan2000@live.com>
2023-03-03 04:09:31 +0000

    Tried to fix the about page responsive issue by isolating CSS in to a seperate file

--- GoldenRiver-Laravel/resources/views/aboutus.blade.php
+++ GoldenRiver-Laravel/resources/views/aboutus.blade.php
@@ -7,7 +7,7 @@
 
 @section('css')
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('css/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/about.css')}}">
 @endsection
 
 
