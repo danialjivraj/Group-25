756c60a334bcc0d4a50b0e7a96f2fa17ac769ff6
rayyan2000 <rayyankhan2000@live.com>
2023-03-04 05:29:55 +0000

    Added three more sections to the new about us page and made changes to the old content

--- GoldenRiver-Laravel/resources/views/aboutus.blade.php
+++ GoldenRiver-Laravel/resources/views/aboutus.blade.php
@@ -24,7 +24,14 @@
       <section class="about-us">
         <div class="about-us-content">
           <h2>Our Story</h2>
-          <p>We are a family-owned and operated business that has been creating and selling high-quality jewelry for over 50 years. Our passion for jewelry started as a hobby, but soon turned into a full-fledged business.</p>
+          <p>We are a group of students who have come together to share our passion 
+            for jewellery to the rest of the world. We as a company have spent a lot of our
+             time into perfecting our craft and creating unique designs that would appeal to
+              a wide range of customers regardless of who you are and what you are looking for. Whether you are looking for a classic traditional piece or something that is loud and unique,
+              our high-quality collection of jewellery has something for everyone. Our determination in design, 
+              craftsmanship and sustainability sets us apart from the
+            rest and makes our jewellery the best choice for those who appreciate quality and style. 
+</h4></p>
   
           <h2>Our Mission</h2>
           <p>Our mission is to provide our customers with the best possible experience when it comes to buying jewelry. We offer a wide selection of styles, materials, and price points to meet the needs of every customer. We pride ourselves on our exceptional customer service and attention to detail.</p>
