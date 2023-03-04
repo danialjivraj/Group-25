d7e2d61e7841c017671fc5ee8931994e32e03d13
rayyan2000 <rayyankhan2000@live.com>
2023-03-04 05:10:39 +0000

    rewrote the html for a new about us page as the old one was un responsive

--- GoldenRiver-Laravel/resources/views/aboutus.blade.php
+++ GoldenRiver-Laravel/resources/views/aboutus.blade.php
@@ -12,25 +12,30 @@
 
 
 @section('body')
-<section class ="about">
-    <div class="main">
-        <img src="images/logo.png">
-        <div class="about-text">
-            <h1>About Us</h1>
-            <h3>Who We Are</h3>
-            <h4> We are a group of students who have come together to share our passion 
-                for jewellery to the rest of the world. We as a company have spent a lot of our
-                 time into perfecting our craft and creating unique designs that would appeal to
-                  a wide range of customers regardless of who you are and what you are looking for. 
-                  Whether you are looking for a classic traditional piece or something that is loud and unique,
-                   our high-quality collection of jewellery has something for everyone. Our determination in design, 
-                   craftsmanship and sustainability sets us apart from the
-                 rest and makes our jewellery the best choice for those of who appreciate quality and style. 
-</h4>
-</div>
-</div>
-</section>
+  <body>
+    <main>
+      <section class="hero">
+        <div class="hero-text">
+          <img src="images/banner.jpg" alt="">
+          <h1>About Us</h1>
+        </div>
+      </section>
+  
+      <section class="about-us">
+        <div class="about-us-content">
+          <h2>Our Story</h2>
+          <p>We are a family-owned and operated business that has been creating and selling high-quality jewelry for over 50 years. Our passion for jewelry started as a hobby, but soon turned into a full-fledged business.</p>
+  
+          <h2>Our Mission</h2>
+          <p>Our mission is to provide our customers with the best possible experience when it comes to buying jewelry. We offer a wide selection of styles, materials, and price points to meet the needs of every customer. We pride ourselves on our exceptional customer service and attention to detail.</p>
+  
+          <h2>Our Team</h2>
+          <p>Our team consists of skilled designers, craftspeople, and salespeople who are passionate about jewelry. We work closely together to ensure that each piece of jewelry we sell is of the highest quality and meets our customers' expectations.</p>
+        </div>
+      </section>
+    </main>
+  
+  </body>
 
-@endsection
-</body>
+  @endsection
 </html>
