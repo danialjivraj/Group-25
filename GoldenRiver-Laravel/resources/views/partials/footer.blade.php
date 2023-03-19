44881a4e4d20f2dc5d1a87ee58b3b08114efb88d
Azraa Faizar <azraafaizar@gmail.com>
2023-03-19 12:50:55 +0000

    footer styling change

--- GoldenRiver-Laravel/resources/views/partials/footer.blade.php
+++ GoldenRiver-Laravel/resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- footer section starts-->
 @yield('css')
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/navandfooter.css')}}">
 @section('css')
 <!--all the links for style sheets custom and ready made bootstrap-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
