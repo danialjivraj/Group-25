f792d6f24f906e1f53d1c0e1c9fcb38929998bf2
Nazrin Chowdhury <210138054>
2023-03-24 01:29:53 +0000

    changes to product images

--- GoldenRiver-Laravel/resources/views/logIn.blade.php
+++ GoldenRiver-Laravel/resources/views/logIn.blade.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
 @extends('partials.nav')
 
 @section('title')
@@ -7,7 +5,8 @@
 @endsection('title')
 
 @section('css')
-    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="./login.js"></script>
 @section('css')
 
@@ -19,44 +18,50 @@
         {{ session('loginToAddCart') }}
     </div>
 @endif
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="reg__container">
+                <h1 class="form__title">Login</h1>
 
-    <div class="login__container">
-
-<h1 class= "form__title">Login</h1>
-
-@if (session('loginerrmsg'))
-    <div class="alert alert-danger" style = "color:red">
-        {{ session('loginerrmsg') }}
-    </div>
-@endif
+                    @if (session('loginerrmsg'))
+                        <div class="alert alert-danger" style = "color:red">
+                            {{ session('loginerrmsg') }}
+                        </div>
+                    @endif
 
 
-<form class="form" action="/login" method="post" class="login-inputs">
-    @csrf
-    <div class="form__input-group">
-    <input type="email" class="form__input" name="email" placeholder="Username or email"  required/><br>
-    <div class="errorlog">
-                    @error('email')
-                    {{ $message }}
-                    <br>
-                    @enderror
-                </div>
-    <input type="password" class="form__input" name="password" placeholder="Password"  required/><br>
-    <div class="errorlog">
-                    @error('password')
-                    {{ $message }}
-                    <br>
-                    @enderror
-                </div>
+                <form class="form" action="/login" method="post" class="login-inputs">
+                        @csrf
+                        <div class="form__input-group">
+                        <input type="email" class="form__input" name="email" placeholder="Email Address"  required /><br>
+                        <div class="errorlog">
+                                        @error('email')
+                                        {{ $message }}
+                                        <br>
+                                        @enderror
+                        </div>
+                        <div class="form__input-group">
+                        <input type="password" class="form__input" name="password" placeholder="Password"  required/><br>
+                        <div class="errorlog">
+                                        @error('password')
+                                        {{ $message }}
+                                        <br>
+                                        @enderror
+                        </div>
+                        </div>
+                        
+                        <button class="form__button" button type="submit">Sign in</button>
+                                    <p class="form__text">
+                                        <a class="form__link" href="./userRegistration" id="linkLogin">Don't have an Account? Sign up</a>
+                                    </p>
+                                    
+                </form>
+            </div>
+        </div>
+    </div> 
 </div>
 
-    <button class="form__button" button type="submit">Sign in</button>
-                <p class="form__text">
-                <a class="form__link" href="./userRegistration" id="linkLogin">Don't have an Account? Sign up</a>
-</p>
-</form>
-
-</div>
 </html>
 
 @endsection
--- GoldenRiver-Laravel/resources/views/userRegistration.blade.php
+++ GoldenRiver-Laravel/resources/views/userRegistration.blade.php
@@ -6,58 +6,66 @@
 
 @section('css')
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 @section('css')
 
 
 @section('body')
-<div class="reg__container">
-    <h1 class="form__title">Registration</h1>
-    <form class="form" form action="/userRegistration" method="post" class="signin-inputs" id="registration">
-        @csrf
-        <div class="form__input-group">
-            <input type="text" class="form__input" name="Fullname" placeholder="Full Name" required /><br>
-            <div class="errorlog">
-                @error('Fullname')
-                {{$message}}
-                <br>
-                @enderror
-            </div>
-        </div>
-        <div class="form__input-group">
-            <input type="email" class="form__input" name="email" placeholder="Your email" required /><br>
-            <div class="errorlog">
-                @error('email')
-                {{$message}}
-                <br>
-                @enderror
-            </div>
-        </div>
-        <div class="form__input-group">
-            <input type="password" class="form__input" name="password" placeholder="Password" required /><br>
-            <div class="errorlog">
-                @error('password')
-                {{$message}}
-                <br>
-                @enderror
-            </div>
-        </div>
-        <div class="form__input-group">
-            <input type="password" class="form__input" name="password_confirmation" placeholder="Retype Password" required /><br>
-            <div class="errorlog">
-                @error('password_confirmation')
-                {{$message}}
-                <br>
-                @enderror
-            </div>
-        </div>
-        <br>
-        <button class="form__button" button type="submit">Register</button>
-        <p class="form__text">
-            <a class="form__link" href="./login" id="linkLogin">Already have an account? Sign in</a>
-        </p>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12"> 
+            <div class="reg__container">
+                <h1 class="form__title">Registration</h1>
+                <form class="form" form action="/userRegistration" method="post" class="signin-inputs" id="registration">
+                    @csrf
+                    <div class="form__input-group">
+                        <input type="text" class="form__input" name="Fullname" placeholder="Full Name" required /><br>
+                        <div class="errorlog">
+                            @error('Fullname')
+                            {{$message}}
+                            <br>
+                            @enderror
+                        </div>
+</div>
+                    <div class="form__input-group">
+                        <input type="email" class="form__input" name="email" placeholder="Your email" required /><br>
+                        <div class="errorlog">
+                            @error('email')
+                            {{$message}}
+                            <br>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form__input-group">
+                        <input type="password" class="form__input" name="password" placeholder="Password" required /><br>
+                        <div class="errorlog">
+                            @error('password')
+                            {{$message}}
+                            <br>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form__input-group">
+                        <input type="password" class="form__input" name="password_confirmation" placeholder="Retype Password" required /><br>
+                        <div class="errorlog">
+                            @error('password_confirmation')
+                            {{$message}}
+                            <br>
+                            @enderror
+                        </div>
+                    </div>
+                    <br>
+                    <button class="form__button" button type="submit">Register</button>
+                    <p class="form__text">
+                        <a class="form__link" href="./login" id="linkLogin">Already have an account? Sign in</a>
+                    </p>
 
 
-    </form>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
 </html>
