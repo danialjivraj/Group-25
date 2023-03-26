caf9c56279f8a781ad2fecab32ada3d45282cf62
Danial Jivraj <danialjivraj@gmail.com>
2023-03-26 22:02:49 +0100

    shop responsiveness improved

--- GoldenRiver-Laravel/resources/views/product.blade.php
+++ GoldenRiver-Laravel/resources/views/product.blade.php
@@ -14,44 +14,6 @@
 @endsection
 
 @section('body')
-<!--VERSION 1
-  <div class="filter-container">
-    <div class="search-container">
-        <form class="search-form" type="get" action="{{ url('/product') }}">
-            @csrf
-            <div class="search-box">
-                <input class="search-input" type="text" name="search" id="search" name="search" placeholder="Search...">
-                <button class="search-submit" type="submit"><i class="fa fa-search"></i></button>
-            </div>
-        </form>
-    </div>
-
-    <hr>
-    <a href="{{ URL::current() }}" class="filter-option">All</a>
-    <a href="{{ URL::current()."?sort=price_ascending" }}" class="filter-option">Price - Ascending</a>
-    <a href="{{ URL::current()."?sort=price_descending" }}" class="filter-option">Price - Descending</a>
-    <a href="{{ URL::current()."?sort=prod_cat" }}" class="filter-option">Category</a>
-    <a href="{{ URL::current()."?sort=popularity" }}" class="filter-option">Popularity</a>
-
-    <form method="get" action="{{ 'product' }}" class="filter-form">
-        <label for="category-select" class="form-label">Category:</label>
-        <select name="filter_by_category" id="category-select" class="form-select">
-            <option value="5">Earrings</option>
-            <option value="6">Necklace</option>
-            <option value="7">Bracelets</option>
-            <option value="8">Rings</option>
-            <option value="9">Exclusive Sets</option>
-        </select>
-        <label for="stock-checkbox" class="form-label">In Stock:</label>
-        <input type="checkbox" id="stock-checkbox" name="filter_by_stock" value="1" class="form-checkbox">
-
-        <button type="submit" class="form-button">Filter</button>
-        <a href="/product" class="form-link">Clear Filters</a>
-    </form>
-</div>
-<hr>
--->
-
 
 
 <hr><!--VERSION 2: bootstrap used to make responsive-->
@@ -99,18 +61,13 @@
             </form>
 
         </div>
-        <!-- <div class="col-sm-12 col-md-12 col-lg-2">sec 4-->
-        <!-- <form method="get" action="{{ 'product' }}" class="filter-form">
-                    <button type="submit" class="form-button">Filter</button>
-                    <a href="/product" class="form-link">Clear Filters</a>
-                </form>
-            </div>  -->
+
     </div>
 
 </div>
 <hr>
 
-<!--<div class="container">-->
+
 <div class="product-row">
     @if(count($products) == 0)
     <h1>No Results Found</h1>
@@ -159,7 +116,7 @@
     @endforeach
     @endif
 </div>
-<!--</div>-->
+
 <div class="pagination-container">
     {{ $products->links("pagination::bootstrap-4") }}
 </div>
