4550ae747fcce2357be4c8d8e12d2f74394252e5
rayyan2000 <rayyankhan2000@live.com>
2023-03-30 17:05:46 +0100

    Made minor changes to the CSS of delivery and private policy pages

--- GoldenRiver-Laravel/resources/views/delivery.blade.php
+++ GoldenRiver-Laravel/resources/views/delivery.blade.php
@@ -6,7 +6,7 @@
 @endsection('title')
 @section('css')
 
-<link rel="stylesheet" href="{{asset('css/delivery.css')}}">
+<link rel="stylesheet" href="{{asset('css/privacy.css')}}">
 @endsection
 @section('body')
 
